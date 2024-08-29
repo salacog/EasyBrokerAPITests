bc8f8cd0a188fe5a0c6ff86f5cae5b6d624f6d5f
Gerardo Salinas <salinasacog@gmail.com>
2024-08-29 03:46:31 -0600

    First trial, API response

---
+++ request.rb
@@ -0,0 +1,21 @@
+require 'uri'
+require 'json'
+require 'net/http'
+
+puts "Listed items: "
+
+#there are 1104 items according to JSON response, we divide by 50 items per page, hence we have to do 23 iterations
+
+url = URI("https://api.stagingeb.com/v1/properties?page=1&limit=20")
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+
+request = Net::HTTP::Get.new(url)
+request["accept"] = 'application/json'
+request["X-Authorization"] = 'l7u502p8v46ba3ppgvj5y2aad50lb9'
+
+response = http.request(request).read_body
+parsed =  JSON.parse(response)
+for i in 0..19
+	puts "-" + parsed['content'][i]['title']
+end
